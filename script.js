a0ad6b3308c70d0a8d519e08f3d48e10318ccb12
Jacquez Patterson <jacquezpatt@gmail.com>
2021-09-23 09:29:20 -0400

    added next question functions

--- script.js
+++ script.js
@@ -4,14 +4,42 @@ var Q2 = document.querySelector("#Q2");
 var Q3 = document.querySelector("#Q3");
 var Q4 = document.querySelector("#Q4");
 var Q5 = document.querySelector("#Q5");
-var cursor = 2
+var cursor = 0
 var questions = [Q1, Q2, Q3, Q4, Q5];
-console.log(questions)
-
+var answer1 = document.querySelectorAll(".answer1");
+var answer2 = document.querySelectorAll(".answer2");
+var answer3 = document.querySelectorAll(".answer3");
+var answer4 = document.querySelectorAll(".answer4");
+var answer5 = document.querySelectorAll(".answer5");
 
 startButton.addEventListener("click", function () {
-    questions[cursor].setAttribute("style", "display: block")
+    next()
     startButton.setAttribute("style", "display: none")
 });
 
+for (var i = 0; i < answer1.length; i++) {
+    answer1[i].addEventListener("click", function () {
+        showNext();
+    });
+}
+for (var i = 0; i < answer2.length; i++) {
+    answer2[i].addEventListener("click", showNext);
+}
 
+for (var i = 0; i < answer3.length; i++) {
+    answer3[i].addEventListener("click", showNext);
+}
+for (var i = 0; i < answer4.length; i++) {
+    answer4[i].addEventListener("click", showNext);
+}
+
+function showNext() {
+    questions[cursor].setAttribute("style", "display: none")
+    cursor++
+    questions[cursor].setAttribute("style", "display: block")
+};
+
+function next() {
+    questions[cursor].setAttribute("style", "display: block")
+
+};
