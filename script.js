067a864a7469736b6b031ce2807d42436693c275
Jacquez Patterson <jacquezpatt@gmail.com>
2021-10-20 16:15:27 -0400

    refresh

--- script.js
+++ script.js
@@ -11,35 +11,10 @@ var answer2 = document.querySelectorAll(".answer2");
 var answer3 = document.querySelectorAll(".answer3");
 var answer4 = document.querySelectorAll(".answer4");
 var answer5 = document.querySelectorAll(".answer5");
+var main = document.querySelector("main");
+var score = 0
+var scoreCard = document.querySelector(".scoreCard")
+var scorePage = document.getElementById("scorePage")
 
-startButton.addEventListener("click", function () {
-    next()
-    startButton.setAttribute("style", "display: none")
-});
 
-for (var i = 0; i < answer1.length; i++) {
-    answer1[i].addEventListener("click", function () {
-        showNext();
-    });
-}
-for (var i = 0; i < answer2.length; i++) {
-    answer2[i].addEventListener("click", showNext);
-}
 
-for (var i = 0; i < answer3.length; i++) {
-    answer3[i].addEventListener("click", showNext);
-}
-for (var i = 0; i < answer4.length; i++) {
-    answer4[i].addEventListener("click", showNext);
-}
-
-function showNext() {
-    questions[cursor].setAttribute("style", "display: none")
-    cursor++
-    questions[cursor].setAttribute("style", "display: block")
-};
-
-function next() {
-    questions[cursor].setAttribute("style", "display: block")
-
-};
